7dcf33943eef7292377d89e5f98c679974db454a
Alessandro Rodi <alessandro.rodi@renuo.ch>
2025-11-03 09:49:01 +0100

    fixes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-import "lexxy"
+import "trix"
 import "@rails/actiontext"
 import LocalTime from "local-time"
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "lexxy", to: "lexxy.js"
+pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "@rails/activestorage", to: "activestorage.esm.js" # to support attachments
 pin "local-time" # @3.0.3
