22116e531301324f783132382f2dfd0f0d931c87
Alessandro Rodi <alessandro.rodi@renuo.ch>
2025-11-03 09:38:25 +0100

    first release

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+import "lexxy"
+import "@rails/actiontext"
+import LocalTime from "local-time"
+
+LocalTime.start()
+document.addEventListener("turbo:morph", () => {
+    LocalTime.run()
+})
